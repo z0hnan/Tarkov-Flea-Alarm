8c714059f504abd649a2d84f3c7ed6acb850332a
z0hnan <simesim2006@gmail.com>
2023-12-14 15:30:04 +0100

    v2.1.2 foolproofed a lot of exceptions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #pip install discord
 import discord
-import datetime
 import asyncio
 import requests
 import json
@@ -22,80 +21,6 @@ def run_query(query):
         return response.json()
     else:
         raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
-
-async def ping(message,msgSplit, pingCounter):
-    myChannel = message.channel
-    nameList = []
-    for x in range(len(msgSplit)):
-        if msgSplit[x] == "<" or msgSplit[x]==">":
-            break
-        else:
-            if msgSplit[x] != "Ping":
-                nameList.append(msgSplit[x])
-    input =" ".join(nameList)
-    if pingCounter > 96:
-        await myChannel.send(f'The ping limit has been reached for "{input}", the tracking limit is 48 hours')
-        pingCounter = 0
-        return
-    new_query = f"""
-    {{
-        items(name: "{input}") {{
-            name
-            sellFor {{
-                price
-                source
-            }}
-        }}
-    }}
-    """
-    result = run_query(new_query)
-     
-    # Access the JSON response directly without replacing
-    items_list = result.get('data', {}).get('items', [])
-
-    # Initialize item_name with None
-    item_name = None
-    # Initialize flea_market_price with None
-    flea_market_price = None
-
-    # Check if the 'items' list is not empty
-    if items_list:
-        # Take the first item (assuming there's only one)
-        data = items_list[0]
-
-        # Extract the item name
-        item_name = data.get('name')
-
-        # Extract the price from the source 'fleaMarket'
-        flea_market_price = next(
-            (item.get('price') for item in data.get('sellFor', []) if item.get('source') == 'fleaMarket'),
-            None
-        )
-    if item_name is not None:
-        printName = item_name
-    else:
-        printName = input
-
-    if msgSplit[len(nameList) + 1] == "<":
-        if float(flea_market_price) < float(msgSplit[len(nameList) + 2]):
-            botResponse = str("<@" + str(message.author.id) + ">" + " The price on the Flea market for "+ printName +" is now "+ str(flea_market_price))
-            await message.channel.send(botResponse)
-        else:
-            pingCounter = pingCounter + 1
-            await asyncio.sleep(1800)
-            await ping(message,msgSplit, pingCounter)
-    elif msgSplit[len(nameList) + 1] == ">":
-        if float(flea_market_price) > float(msgSplit[len(nameList) + 2]):
-            botResponse = str("<@" + str(message.author.id) + ">" + " The price on the Flea market for "+ printName +" is now "+ str(flea_market_price))
-            await message.channel.send(botResponse)
-        else:
-            pingCounter = pingCounter + 1
-            await asyncio.sleep(1800)
-            await ping(message,msgSplit, pingCounter)
-    else:
-        await message.channel.send("Invalid input please use this format: Ping *item name* < or > *price* for example: Ping Metal Fuel < 1000")
-
-
 
 async def handleMsg(message,msgSplit):
     input =" ".join(msgSplit[1:])
@@ -147,6 +72,86 @@ async def handleMsg(message,msgSplit):
     else:
         await message.channel.send("That item is not on the Flea Market or a skill issue ocurred")
 
+
+async def ping(message,msgSplit, pingCounter):
+    myChannel = message.channel
+    nameList = []
+    for x in range(len(msgSplit)):
+        if msgSplit[x] == "<" or msgSplit[x]==">":
+            break
+        else:
+            if msgSplit[x] != "Ping":
+                nameList.append(msgSplit[x])
+    input =" ".join(nameList)
+    if pingCounter > 96:
+        await myChannel.send(f'The ping limit has been reached for "{input}", the tracking limit is 48 hours')
+        pingCounter = 0
+        return
+    new_query = f"""
+    {{
+        items(name: "{input}") {{
+            name
+            sellFor {{
+                price
+                source
+            }}
+        }}
+    }}
+    """
+    result = run_query(new_query)
+     
+    # Access the JSON response directly without replacing
+    items_list = result.get('data', {}).get('items', [])
+
+    # Initialize item_name with None
+    item_name = None
+    # Initialize flea_market_price with None
+    flea_market_price = None
+
+    # Check if the 'items' list is not empty
+    if items_list:
+        # Take the first item (assuming there's only one)
+        data = items_list[0]
+
+        # Extract the item name
+        item_name = data.get('name')
+
+        # Extract the price from the source 'fleaMarket'
+        flea_market_price = next(
+            (item.get('price') for item in data.get('sellFor', []) if item.get('source') == 'fleaMarket'),
+            None
+        )
+    #Tests if real name exists
+    if item_name is not None:
+        printName = item_name
+    else:
+        printName = input
+    #Sends message to discord if price couldn't be found
+    if flea_market_price is None:
+        await message.channel.send(f"{input} is not on the Flea Market or a skill issue ocurred. Please use this format: Ping *item name* < or > *price* for example: Ping Metal Fuel < 1000")
+        return
+       
+
+    if msgSplit[len(msgSplit)-2] == "<":
+        if float(flea_market_price) < float(msgSplit[len(msgSplit)-1]):
+            botResponse = str("<@" + str(message.author.id) + ">" + " The price on the Flea market for "+ printName +" is now "+ str(flea_market_price))
+            await message.channel.send(botResponse)
+        else:
+            pingCounter = pingCounter + 1
+            await asyncio.sleep(1800)
+            await ping(message,msgSplit, pingCounter)
+    elif msgSplit[len(msgSplit)-2] == ">":
+        if float(flea_market_price) > float(msgSplit[len(msgSplit)-1]):
+            botResponse = str("<@" + str(message.author.id) + ">" + " The price on the Flea market for "+ printName +" is now "+ str(flea_market_price))
+            await message.channel.send(botResponse)
+        else:
+            pingCounter = pingCounter + 1
+            await asyncio.sleep(1800)
+            await ping(message,msgSplit, pingCounter)
+    else:
+        await message.channel.send("Invalid input please use this format: Ping *item name* < or > *price* for example: Ping Metal Fuel < 1000")
+
+
 #On Log in
 @client.event
 async def on_ready():
@@ -158,10 +163,16 @@ async def on_ready():
         if message.author == client.user:
             return
         msgSplit = message.content.split()
-        if msgSplit[0] =="Fleabot":
-            await handleMsg(message,msgSplit)
-        if msgSplit[0] =="Ping":
-            await ping(message,msgSplit, pingCounter)
+        if msgSplit[0].lower() =="fleabot":
+            if len(msgSplit)!=1:
+                await handleMsg(message,msgSplit)
+            else:
+                await message.channel.send("Please enter an item name after Fleabot")
+        if msgSplit[0].lower() =="ping":
+            if len(msgSplit)!=1:
+                await ping(message,msgSplit, pingCounter)
+            else:
+                await message.channel.send("Invalid input please use this format: Ping *item name* < or > *price* for example: Ping Metal Fuel < 1000")
 
 #Add your discord bot token    
-client.run('---YOUR BOT TOKEN HERE---')
+client.run('MTE0NjAwMTI4NTY5MTIzMjI5Ng.G5GHeX.2oMrM-0QPm0ahmYWmuuqGcz86lS-o1Gy2nq6O8')
